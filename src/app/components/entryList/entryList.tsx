f489769617d5e00539d070ab4166f95bdefe6bc1
T. Bank <tommy.bank@hva.nl>
2024-12-01 10:13:09 +0100

    fix: change time to 12 and remove unused code

--- src/app/components/entryList/entryList.tsx
+++ src/app/components/entryList/entryList.tsx
@@ -3,17 +3,10 @@ import EntryType from "@/app/types/enums/entryType";
 import EntryItem from "./entryItem/entryItem";
 import styles from "./entryList.module.css";
 import Entry from "@/app/types/models/entry";
-import CircleButton from "../buttons/circleButton/circleButton";
-import { useRouter } from "next/navigation";
 import AddEntry from "../addEntry/addEntry";
 import { useEffect, useState } from "react";
 import TextButton from "../buttons/textButton/textButton";
 import { Spinner } from "flowbite-react";
-
-
-function isValidEntryType(value: any): value is EntryType {
-    return Object.values(EntryType).includes(value);
-}
 
 interface EntryListProps {
     setPage: Function;
@@ -22,11 +15,13 @@ interface EntryListProps {
 
 const EntryList: React.FC<EntryListProps> = ({setPage, date}) => {
     const [entries, setEntries] = useState<Entry[]>([]);
-    const router = useRouter();
     const [loading, setLoading] = useState(true);
 
     async function getEntries() {
         const day = new Date(date);
+        day.setHours(12);
+        day.setMinutes(12);
+        day.setSeconds(12);
 
         const response = await fetch("../api/entry/get-entries", {
             method: "POST",
@@ -47,6 +42,7 @@ const EntryList: React.FC<EntryListProps> = ({setPage, date}) => {
             console.log(response);
         }
     }
+    
 
     useEffect(() => {
         getEntries();
