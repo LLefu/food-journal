4d79b0603f06e2393564f01ec295c149e0f579e6
Tommy Bank <tommy@arlanet.com>
2024-12-04 13:53:42 +0100

    fix: footer nav color

--- src/app/components/navMenu/navMenu.tsx
+++ src/app/components/navMenu/navMenu.tsx
@@ -2,7 +2,6 @@
 import styles from "./navMenu.module.css";
 import Calendar from '../calendar/calendar';
 import Today from '../today/today';
-import { useState } from "react";
 
 interface NavMenuProps {
   setPage: Function;
@@ -12,14 +11,13 @@ interface NavMenuProps {
 }
 
 const NavMenu: React.FC<NavMenuProps> = ({setPage, currentPage, goDatePage, setTitle}) => {
-  const [pageSwitcher, setPageSwitcher] = useState(true);
 
   return <div className={styles.navMenu}>
-    <div onClick={()=>{setPage(<Today setTitle={setTitle} setPage={setPage}/>); setPageSwitcher(true)}} className={styles.navItem}>
-      <i className={`fa fa-list ${styles.icon} ${pageSwitcher ? styles.isPage : "" }`}></i>
+    <div onClick={()=>{setPage(<Today setTitle={setTitle} setPage={setPage}/>); setTitle("Today")}} className={styles.navItem}>
+      <i className={`fa fa-list ${styles.icon} ${currentPage.type.name == "Today" ? "text-neutral-400" : "" }`}></i>
     </div>
-    <div onClick={()=>{setPage(<Calendar setDate={goDatePage} setTitle={setTitle} />); setPageSwitcher(false); }} className={styles.navItem}>
-      <i className={`fa fa-calendar ${styles.icon} ${!pageSwitcher ? styles.isPage : "" }`}></i>
+    <div onClick={()=>{setPage(<Calendar setDate={goDatePage} setTitle={setTitle}/>); setTitle("Calendar")}} className={styles.navItem}>
+      <i className={`fa fa-calendar ${styles.icon} ${currentPage.type.name == "Calendar" ? "text-neutral-400" : "" }`}></i>
     </div>
   </div>;
 };
